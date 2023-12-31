e1e304ca31763000434193ac9134510c24c33ba7
Paritosh Tiwari <paritoshtiwari2801@gmail.com>
2023-12-31 12:10:43 +0530

    Add files via upload

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -6,13 +6,30 @@ from exceptions.exceptions import InvalidBackboneError
 
 class ResNetSimCLR(nn.Module):
 
-    def __init__(self, base_model, out_dim):
+    def __init__(self, base_model, out_dim, grayscale=False):
         super(ResNetSimCLR, self).__init__()
         self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
-                            "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)}
+                            "resnet34": models.resnet34(pretrained=False, num_classes=out_dim),  # Added ResNet34
+                            "resnet50": models.resnet50(pretrained=False, num_classes=out_dim),
+                            "resnet101": models.resnet101(pretrained=False, num_classes=out_dim),  # Added ResNet101
+                            "resnet152": models.resnet152(pretrained=False, num_classes=out_dim)  # Added ResNet152
+                            }
 
         self.backbone = self._get_basemodel(base_model)
+
+        if grayscale:
+            # Modify the first convolutional layer to accept 1-channel images
+            self.backbone.conv1 = nn.Conv2d(1, self.backbone.conv1.out_channels,
+                                            kernel_size=self.backbone.conv1.kernel_size,
+                                            stride=self.backbone.conv1.stride,
+                                            padding=self.backbone.conv1.padding,
+                                            bias=False)
+
+
         dim_mlp = self.backbone.fc.in_features
+
+        # dim_mlp = self.backbone.fc.in_features
+        # self.backbone.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), nn.Linear(dim_mlp, out_dim))
 
         # add mlp projection head
         self.backbone.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.fc)
@@ -22,7 +39,7 @@ class ResNetSimCLR(nn.Module):
             model = self.resnet_dict[model_name]
         except KeyError:
             raise InvalidBackboneError(
-                "Invalid backbone architecture. Check the config file and pass one of: resnet18 or resnet50")
+                "Invalid backbone architecture. Choose one of: resnet18, resnet34, resnet50, resnet101, or resnet152.")
         else:
             return model
 
