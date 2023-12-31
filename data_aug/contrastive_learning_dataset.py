4085ede9cd410234a0b32b185285077949839709
Paritosh Tiwari <paritoshtiwari2801@gmail.com>
2023-12-31 12:09:23 +0530

    Add files via upload

--- data_aug/contrastive_learning_dataset.py
+++ data_aug/contrastive_learning_dataset.py
@@ -3,36 +3,77 @@ from data_aug.gaussian_blur import GaussianBlur
 from torchvision import transforms, datasets
 from data_aug.view_generator import ContrastiveLearningViewGenerator
 from exceptions.exceptions import InvalidDatasetSelection
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import Grayscale
 
 
 class ContrastiveLearningDataset:
-    def __init__(self, root_folder):
+    def __init__(self, root_folder, crop_size=32):
         self.root_folder = root_folder
+        self.crop_size = crop_size
 
-    @staticmethod
-    def get_simclr_pipeline_transform(size, s=1):
+    # @staticmethod
+    # def get_simclr_pipeline_transform(size, s=1):
+    #     """Return a set of data augmentation transformations as described in the SimCLR paper."""
+    #     color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    #     data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=size),
+    #                                           transforms.RandomHorizontalFlip(),
+    #                                           transforms.RandomApply([color_jitter], p=0.8),
+    #                                           transforms.RandomGrayscale(p=0.2),
+    #                                           GaussianBlur(kernel_size=int(0.1 * size)),
+    #                                           transforms.ToTensor()])
+    #     return data_transforms
+    
+    # @staticmethod
+    def get_simclr_pipeline_transform(self, s=1, is_grayscale=False):
         """Return a set of data augmentation transformations as described in the SimCLR paper."""
-        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=size),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply([color_jitter], p=0.8),
-                                              transforms.RandomGrayscale(p=0.2),
-                                              GaussianBlur(kernel_size=int(0.1 * size)),
-                                              transforms.ToTensor()])
-        return data_transforms
+        transformations = [transforms.RandomResizedCrop(size=self.crop_size),
+                           transforms.RandomHorizontalFlip()]
+
+        if not is_grayscale:
+            # Color jitter only for RGB images
+            color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+            transformations.append(transforms.RandomApply([color_jitter], p=0.8))
+            transformations.append(transforms.RandomGrayscale(p=0.2))
+
+        # Gaussian blur can be applied to both grayscale and RGB
+        transformations.append(GaussianBlur(kernel_size=int(0.1 * self.crop_size)))
+        transformations.append(transforms.ToTensor())
+
+        return transforms.Compose(transformations)
 
     def get_dataset(self, name, n_views):
         valid_datasets = {'cifar10': lambda: datasets.CIFAR10(self.root_folder, train=True,
                                                               transform=ContrastiveLearningViewGenerator(
-                                                                  self.get_simclr_pipeline_transform(32),
+                                                                  self.get_simclr_pipeline_transform(),
                                                                   n_views),
                                                               download=True),
 
                           'stl10': lambda: datasets.STL10(self.root_folder, split='unlabeled',
                                                           transform=ContrastiveLearningViewGenerator(
-                                                              self.get_simclr_pipeline_transform(96),
+                                                              self.get_simclr_pipeline_transform(),
                                                               n_views),
-                                                          download=True)}
+                                                          download=True),
+                                                          
+                            # Custom RGB Dataset
+                            'custom_rgb': lambda: ImageFolder(
+                                self.root_folder,
+                                transform=ContrastiveLearningViewGenerator(
+                                    self.get_simclr_pipeline_transform(),  # Adjust size as needed
+                                    n_views)
+                            ),
+
+                            # Custom Grayscale Dataset
+                            'custom_grayscale': lambda: ImageFolder(
+                                self.root_folder,
+                                transform=transforms.Compose([
+                                    Grayscale(num_output_channels=1),
+                                    ContrastiveLearningViewGenerator(
+                                        self.get_simclr_pipeline_transform(is_grayscale=True),  # Adjust size as needed
+                                        n_views)
+                                ])
+                            )
+                        }
 
         try:
             dataset_fn = valid_datasets[name]
--- data_aug/gaussian_blur.py
+++ data_aug/gaussian_blur.py
@@ -6,23 +6,54 @@ from torchvision.transforms import transforms
 np.random.seed(0)
 
 
+# class GaussianBlur(object):
+#     """blur a single image on CPU"""
+#     def __init__(self, kernel_size):
+#         radias = kernel_size // 2
+#         kernel_size = radias * 2 + 1
+#         self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
+#                                 stride=1, padding=0, bias=False, groups=3)
+#         self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
+#                                 stride=1, padding=0, bias=False, groups=3)
+#         self.k = kernel_size
+#         self.r = radias
+
+#         self.blur = nn.Sequential(
+#             nn.ReflectionPad2d(radias),
+#             self.blur_h,
+#             self.blur_v
+#         )
+
+#         self.pil_to_tensor = transforms.ToTensor()
+#         self.tensor_to_pil = transforms.ToPILImage()
+
+#     def __call__(self, img):
+#         img = self.pil_to_tensor(img).unsqueeze(0)
+
+#         sigma = np.random.uniform(0.1, 2.0)
+#         x = np.arange(-self.r, self.r + 1)
+#         x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
+#         x = x / x.sum()
+#         x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
+
+#         self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
+#         self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
+
+#         with torch.no_grad():
+#             img = self.blur(img)
+#             img = img.squeeze()
+
+#         img = self.tensor_to_pil(img)
+
+#         return img
+    
 class GaussianBlur(object):
-    """blur a single image on CPU"""
+    """ Blur a single image on CPU """
     def __init__(self, kernel_size):
         radias = kernel_size // 2
         kernel_size = radias * 2 + 1
-        self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
-                                stride=1, padding=0, bias=False, groups=3)
-        self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
-                                stride=1, padding=0, bias=False, groups=3)
         self.k = kernel_size
         self.r = radias
-
-        self.blur = nn.Sequential(
-            nn.ReflectionPad2d(radias),
-            self.blur_h,
-            self.blur_v
-        )
 
         self.pil_to_tensor = transforms.ToTensor()
         self.tensor_to_pil = transforms.ToPILImage()
@@ -30,17 +61,29 @@ class GaussianBlur(object):
     def __call__(self, img):
         img = self.pil_to_tensor(img).unsqueeze(0)
 
+        # Dynamically adjust the convolutional layers based on the number of channels
+        num_channels = img.shape[1]
+        blur_h = nn.Conv2d(num_channels, num_channels, kernel_size=(self.k, 1),
+                           stride=1, padding=0, bias=False, groups=num_channels)
+        blur_v = nn.Conv2d(num_channels, num_channels, kernel_size=(1, self.k),
+                           stride=1, padding=0, bias=False, groups=num_channels)
+        blur = nn.Sequential(
+            nn.ReflectionPad2d(self.r),
+            blur_h,
+            blur_v
+        )
+
         sigma = np.random.uniform(0.1, 2.0)
         x = np.arange(-self.r, self.r + 1)
         x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
         x = x / x.sum()
-        x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
+        x = torch.from_numpy(x).view(1, -1).repeat(num_channels, 1)
 
-        self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
-        self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
+        blur_h.weight.data.copy_(x.view(num_channels, 1, self.k, 1))
+        blur_v.weight.data.copy_(x.view(num_channels, 1, 1, self.k))
 
         with torch.no_grad():
-            img = self.blur(img)
+            img = blur(img)
             img = img.squeeze()
 
         img = self.tensor_to_pil(img)
